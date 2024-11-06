b9033c3f1bfd87118c803e579ff157220ed341e0
xZcAtliftz Project <99465722+xZcAtliftz@users.noreply.github.com>
2024-11-06 19:33:17 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,30 @@
+if _G.KaitunMode then
+    local gamelist = {
+        [994732206] = "https://raw.githubusercontent.com/xZcAtliftz/Seraphic-Scripts/main/Kaitun.lua"
+    }
+    local checkgame = gamelist[game.gameId]
 
+    if gamelist[game.gameId] then
+        getgenv().AuthTime = tick()
+        print(checkgame)
+        loadstring(game:HttpGet(checkgame))()
+    else
+        game.Players.LocalPlayer:Kick("Game Not Support")
+    end
+else
+    local gamelist = {
+        [4777817887] = "https://raw.githubusercontent.com/xZcAtliftz/Data-Hub/main/Blade-Ball.lua", -- BB
+        [5750914919] = "https://raw.githubusercontent.com/xZcAtliftz/Data-Hub/main/Fisch.lua", -- Fisch
+        [994732206] = "https://raw.githubusercontent.com/xZcAtliftz/Data-Hub/main/Blox-Fruits.lua", -- BF
+        [1202096104] = "https://raw.githubusercontent.com/xZcAtliftz/Data-Hub/main/Driving-Empire.lua", -- DE
+    }
+    local checkgame = gamelist[game.gameId]
+
+    if gamelist[game.gameId] then
+        getgenv().AuthTime = tick()
+        print(checkgame)
+        loadstring(game:HttpGet(checkgame))()
+    else
+        game.Players.LocalPlayer:Kick("Game Not Support")
+    end
+end
